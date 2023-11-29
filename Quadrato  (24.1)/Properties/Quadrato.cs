f18322d24a5c6f63aa150720e706ab8f8b6ddef7
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2023-11-29 21:52:19 +0100

    Aggiungere i file di progetto.

---
+++ Quadrato  (24.1)/Properties/Quadrato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrato___24._1_.Properties
+{
+    internal class Quadrato
+    {
+        public float Lato { get; set; }
+    }
+}
